60f9d3909b5e4f7ccdd3d4bd8efa7cf5de306971
David M <dmozartandraws@gmail.com>
2025-01-07 15:57:52 +0100

    add growatt definitions - part 1

--- src/inverters/__init__.py
+++ src/inverters/__init__.py
@@ -7,6 +7,7 @@ from .fronius.fronius import fronius_profile
 from .solis.solis import solis_profile
 from .solis.solis_hybrid import solis_hybrid_profile
 from .sma.sma import sma_profile
+from .growatt.growatt import growatt_profile
 
 # Registry of supported inverter profiles
 INVERTER_PROFILES: Dict[str, Dict[str, Any]] = {
@@ -18,5 +19,6 @@ INVERTER_PROFILES: Dict[str, Dict[str, Any]] = {
     'solis': solis_profile,
     'solis_hybrid': solis_hybrid_profile,
     'sma': sma_profile,
+    'growatt': growatt_profile,
     # Add more profiles here as they are supported
 }
